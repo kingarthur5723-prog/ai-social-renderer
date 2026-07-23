679df0ff17e1ecbf2870f637cee91007eefbf899
kingarthur5723-prog <kingarthur5723@gmail.com>
2026-07-23 17:34:45 +0100

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,39 +1,37 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const path = require("path");
+const { exec } = require("child_process");
 
 async function downloadImages(images) {
 
-    const folder = path.join(__dirname, "uploads");
-
-    await fs.ensureDir(folder);
+    const uploadDir = path.join(__dirname, "uploads");
+    await fs.ensureDir(uploadDir);
 
     const files = [];
 
     for (let i = 0; i < images.length; i++) {
 
-        const filename = path.join(folder, `scene${i}.jpg`);
+        const file = path.join(uploadDir, `scene${i}.jpg`);
 
         const response = await axios({
-
             url: images[i],
             method: "GET",
             responseType: "stream"
-
         });
 
-        const writer = fs.createWriteStream(filename);
-
-        response.data.pipe(writer);
-
         await new Promise((resolve, reject) => {
+
+            const writer = fs.createWriteStream(file);
+
+            response.data.pipe(writer);
 
             writer.on("finish", resolve);
             writer.on("error", reject);
 
         });
 
-        files.push(filename);
+        files.push(file);
 
     }
 
@@ -41,8 +39,51 @@ async function downloadImages(images) {
 
 }
 
+async function createVideo(images, outputFile) {
+
+    const listFile = path.join(__dirname, "uploads", "list.txt");
+
+    let text = "";
+
+    images.forEach(img => {
+
+        text += `file '${img}'\n`;
+        text += "duration 5\n";
+
+    });
+
+    text += `file '${images[images.length - 1]}'\n`;
+
+    await fs.writeFile(listFile, text);
+
+    return new Promise((resolve, reject) => {
+
+        const cmd =
+`ffmpeg -y \
+-f concat \
+-safe 0 \
+-i "${listFile}" \
+-vf "scale=1080:1920:force_original_aspect_ratio=increase,crop=1080:1920" \
+-r 30 \
+-pix_fmt yuv420p \
+"${outputFile}"`;
+
+        console.log(cmd);
+
+        exec(cmd, (err) => {
+
+            if (err) reject(err);
+            else resolve(outputFile);
+
+        });
+
+    });
+
+}
+
 module.exports = {
 
-    downloadImages
+    downloadImages,
+    createVideo
 
 };
