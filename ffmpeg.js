58a018b0593ae2d4041745491f2e601058673a6b
kingarthur5723-prog <kingarthur5723@gmail.com>
2026-07-23 17:28:58 +0100

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -1,1 +1,48 @@
-coming soon
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+
+async function downloadImages(images) {
+
+    const folder = path.join(__dirname, "uploads");
+
+    await fs.ensureDir(folder);
+
+    const files = [];
+
+    for (let i = 0; i < images.length; i++) {
+
+        const filename = path.join(folder, `scene${i}.jpg`);
+
+        const response = await axios({
+
+            url: images[i],
+            method: "GET",
+            responseType: "stream"
+
+        });
+
+        const writer = fs.createWriteStream(filename);
+
+        response.data.pipe(writer);
+
+        await new Promise((resolve, reject) => {
+
+            writer.on("finish", resolve);
+            writer.on("error", reject);
+
+        });
+
+        files.push(filename);
+
+    }
+
+    return files;
+
+}
+
+module.exports = {
+
+    downloadImages
+
+};
