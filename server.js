cb814cf90b803b3e57c5f870ea07f459007d16ec
kingarthur5723-prog <kingarthur5723@gmail.com>
2026-07-27 06:34:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json({ limit: "50mb" }));
 
 // Ensure folders exist
 fs.ensureDirSync(path.join(__dirname, "uploads"));
+fs.ensureDirSync(path.join(__dirname,"temp"));
 fs.ensureDirSync(path.join(__dirname, "output"));
 fs.ensureDirSync(path.join(__dirname, "assets"));
 
