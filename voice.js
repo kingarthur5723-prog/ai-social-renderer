25d662481ae7f443948205fcb6a819ad7b74ee29
kingarthur5723-prog <kingarthur5723@gmail.com>
2026-07-30 18:42:22 +0100

    Update voice.js

--- voice.js
+++ voice.js
@@ -1,1 +1,24 @@
-type node_modules\edge-tts\README.md
+const { ttsSave } = require("edge-tts");
+const path = require("path");
+const fs = require("fs-extra");
+
+async function generateVoice(text, jobId) {
+    const voiceDir = path.join(__dirname, "voices");
+
+    await fs.ensureDir(voiceDir);
+
+    const output = path.join(voiceDir, `${jobId}.mp3`);
+
+    await ttsSave(text, output, {
+        voice: "en-US-GuyNeural",
+        rate: "+0%",
+        pitch: "+0Hz",
+        volume: "+0%"
+    });
+
+    console.log("Voice created:", output);
+
+    return output;
+}
+
+module.exports = generateVoice;
